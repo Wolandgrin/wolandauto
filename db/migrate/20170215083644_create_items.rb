7f67b5efd48fec6406fa9e946c4d5744f373bf22
Wolandgrin <vladimir.grin@gmail.com>
2017-02-19 18:55:21 +0200

    minor improvements

--- db/migrate/20170215083644_create_items.rb
+++ db/migrate/20170215083644_create_items.rb
@@ -2,11 +2,11 @@ class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
       t.string :name
-      t.float :price
-      t.float :interest
+      t.decimal :price
+      t.decimal :interest
       t.integer :duration
       #t.integer :user_id
-      t.references :users, foreign_key: true
+      t.references :user_id, foreign_key: true
       t.belongs_to :user, index: true
       t.timestamps null: false
     end

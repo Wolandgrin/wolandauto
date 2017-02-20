923a2d47e1a506576ed98cc110f2ec7ad410d98b
Wolandgrin <vladimir.grin@gmail.com>
2017-02-20 11:44:43 +0200

    heroku deploy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,7 +10,7 @@ module SessionsHelper
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  # Returns true if the user is logged in, false otherwise.
+  # Returns true if the user is logged in, false otherwise
   def logged_in?
     !current_user.nil?
   end
